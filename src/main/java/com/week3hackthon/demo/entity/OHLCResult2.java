e49ed3c723f6d41cf5884139cae8bcbd7636a187
BSXRB <782923640@qq.com>
2021-08-27 14:16:11 +0800

    mvn test

--- src/main/java/com/week3hackthon/demo/entity/OHLCResult2.java
+++ src/main/java/com/week3hackthon/demo/entity/OHLCResult2.java
@@ -12,15 +12,7 @@ public class OHLCResult2 {
     Integer week;
     Integer year;
     Integer month;
-    String str = "test";
 
-    public String getStr() {
-        return str;
-    }
-
-    public void setStr(String str) {
-        this.str = str;
-    }
 
     public String getName() {
         return name;
